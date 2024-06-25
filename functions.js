f81ce956fe0b1114965520c75b64d9f83100d15f
jameskiprop <jameskiprop01@gmail.com>
2024-06-25 09:11:08 +0300

    -small exercise on js objects and functions

--- functions.js
+++ functions.js
@@ -1,38 +1,40 @@
-const student = {
-  firstName: "James",
-  lastName: "Calvin",
-  Age: 19,
-};
+const students = [
+  {
+    firstName: "James",
+    lastName: "Calvin",
+    age: 19,
+  },
 
-const student1 = {
-  firstName: "John",
-  lastName: "Doe",
-  Age: 13,
-};
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 13,
+  },
 
-const student2 = {
-  firstName: "Angelina",
-  lastName: "Joe",
-  Age: 26,
-};
+  {
+    firstName: "Angelina",
+    lastName: "Joe",
+    age: 26,
+  },
 
-const student3 = {
-  firstName: "Nicki",
-  lastName: "Stephens",
-  Age: 45,
-};
+  {
+    firstName: "Nicki",
+    lastName: "Stephens",
+    age: 45,
+  },
+];
 
 function studentGeneration(students) {
-  let student = `age`;
-
-  if (student.age > 0 && student.age <= 13)
-    console.log(` ${student.firstName} ${student.lastName} is a Gen Alpha`);
-  else if (student.age > 13 && student.age <= 27)
-    console.log(`${student.firstName} ${student.lastName} is a Gen Z`);
-  else if (student.age > 27 && student.age <= 37)
-    console.log(`${student.firstName} ${student.lastName} is a Millenial`);
-  else {
-    console.log(`that's a boomer`);
-  }
+  students.forEach((s) => {
+    if (s.age >= 0 && s.age <= 13)
+      console.log(`${s.firstName} ${s.lastName} is a Gen Alpha`);
+    else if (s.age > 13 && s.age <= 27)
+      console.log(`${s.firstName} ${s.lastName} is a Gen Z`);
+    else if (s.age > 27 && s.age <= 37)
+      console.log(`${s.firstName} ${s.lastName} is a Millenial`);
+    else {
+      console.log(`that's a boomer`);
+    }
+  });
 }
-studentGeneration([student, student1, student2, student3]);
+studentGeneration(students);
